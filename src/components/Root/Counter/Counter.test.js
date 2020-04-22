a06d33b4217f23481ba794559c6efa929e277dae
vj1008466 <53852764+vj1008466@users.noreply.github.com>
2020-04-22 22:40:20 +0800

    Update Counter.test.js

--- src/components/Root/Counter/Counter.test.js
+++ src/components/Root/Counter/Counter.test.js
@@ -7,10 +7,12 @@ test("Counter component works properly", () => {
   const { getByTestId, getByText } = render(<Counter />);
 
   const counterValueEl = getByTestId("counter-value");
-  #const minusTwoButtonEl = getByText("-2", { selector: "button" });
+  const minusThreeButtonEl = getByText("-3", { selector: "button" });
+  const minusTwoButtonEl = getByText("-2", { selector: "button" });
   const minusOneButtonEl = getByText("-1", { selector: "button" });
   const plusOneButtonEl = getByText("+1", { selector: "button" });
-  #const plusTwoButtonEl = getByText("+2", { selector: "button" });
+  const plusTwoButtonEl = getByText("+2", { selector: "button" });
+  const plusThreeButtonEl = getByText("+3", { selector: "button" });
 
   expect(counterValueEl).toHaveTextContent("0");
 
@@ -21,6 +23,10 @@ test("Counter component works properly", () => {
   fireEvent.click(minusTwoButtonEl);
 
   expect(counterValueEl).toHaveTextContent("-3");
+  
+  fireEvent.click(minusThreeButtonEl);
+
+  expect(counterValueEl).toHaveTextContent("-4");
 
   fireEvent.click(plusOneButtonEl);
 
@@ -33,4 +39,8 @@ test("Counter component works properly", () => {
   fireEvent.click(plusOneButtonEl);
 
   expect(counterValueEl).toHaveTextContent("1");
+  
+    fireEvent.click(plusThreeButtonEl);
+
+  expect(counterValueEl).toHaveTextContent("4");
 });
